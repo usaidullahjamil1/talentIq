4a517e1c3a172443a970feff58700e9461a18b28
Usaid Ullah Jamil <usaidjamil6@gmail.com>
2025-11-26 11:53:34 +0500

    initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+import { ENV } from "./lib/env.js";
+
+const app = express();
+
+console.log("ENV PORT ", ENV.PORT);
+app.get("/", (req, res) => {
+  res.status(200).json({ msg: "Success from api" });
+});
+app.listen(ENV.PORT, () => console.log("Server running on port", ENV.PORT));
