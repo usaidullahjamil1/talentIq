2d840add26d078df5eb47148008bddeae3127181
Usaid Ullah Jamil <usaidjamil6@gmail.com>
2025-11-27 22:25:34 +0500

    db and tools setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { ENV } from "./lib/env.js";
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 const __dirname = path.resolve();
@@ -22,4 +23,16 @@ if (ENV.NODE_ENV == "production") {
     res.sendFile(path.join((__dirname, "../frontend", "dist", "index.html")));
   });
 }
-app.listen(ENV.PORT, () => console.log("Server running on port: ", ENV.PORT));
+
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(ENV.PORT, () => {
+      console.log("Server running on port: ", ENV.PORT);
+    });
+  } catch (error) {
+    console.log("Error starting db:", err);
+  }
+};
+
+startServer();
